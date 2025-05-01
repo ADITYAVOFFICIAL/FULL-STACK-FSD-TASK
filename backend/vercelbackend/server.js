6b0b7b4ddc776712fc8a29df98009b5a73b8406e
ADITYA VERMA <adityav1304@gmail.com>
2025-05-02 01:58:45 +0530

    Update server.js

--- backend/vercelbackend/server.js
+++ backend/vercelbackend/server.js
@@ -11,9 +11,9 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: 'https://teammavericks.vercel.app/*', // Allow requests from your frontend domain
+    origin: '*', // Allow requests from any origin
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allow common methods
-    allowedHeaders: ['Content-Type', 'Authorization','Access-Control-Allow-Origin'] // Allow necessary headers
+    allowedHeaders: ['Content-Type', 'Authorization'] // Allow necessary headers
   }));
   app.use(express.json());
 // Removed static file serving for /uploads, as files won't be stored locally
